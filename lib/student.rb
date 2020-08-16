6af81c92f3019a4720599d24abb1727c77ae45fc
nisayana <nisayana23@yandex.ru>
2020-08-15 23:54:16 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge_str)
+        @knowledge << knowledge_str
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
